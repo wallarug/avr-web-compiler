002374de4e9eb98f5f4f44b14bbc53c2eda34c84
wallarug@rpi <wallarug@bitbucket.org>
2015-11-04 13:28:11 +0000

    commit

--- webavr/avrcompiler/program.s
+++ webavr/avrcompiler/program.s
@@ -6,19 +6,32 @@
 	.global asm_function
 
 asm_function:
+	LDI R16, 9
+	LDI R17, 12
+	LDI R18, 15
+	MUL R16, R16
+	;;ROR R18
+	MOV R16, R0 ;;result of mul in R16
+	MUL R17, R17
+	MOV R17, R0 ;;result of mul in R17
+	MUL R18, R18
+	MOV R18, R0 ;;result of mul in R18
+	ADD R16, R17 ;;result in R16
+	CP R18, R16
 	
-	LDI R16,1
-	LDI R17,2
-	LDI R18,3
-	LDI R19,4
-	LDI R20,5
-	
-	ADD R16,R17
-	ADD R16,R18
-	ADD R16,R19
-	ADD R16,R20
-	
-	MOV R24, R16
-	CLR R25
+	BREQ true
+	BRNE false
+	true:	
+		LDI R16, 1
+		MOV R24, R16
+		CLR R25
+		call print_integer
+		ret
+	false:	
+		LDI R16, 0
+		MOV R24, R16
+		CLR R25
+		call print_integer
+		ret
 	ret
 .end
